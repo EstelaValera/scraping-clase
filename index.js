7110cefa6e3a026614c11bcea65f7b37a5ca56c2
Estela Valera <estela.valera96@gmail.com>
2024-09-04 16:32:36 +0200

    terminado

---
+++ index.js
@@ -0,0 +1,50 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const { link } = require('fs')
+const app = express()
+
+const url = 'https://estelavalera.github.io/project-break-dashboard/'
+
+
+app.get('/', (req, res) => {
+    axios.get(url).then((response) => {
+        if(response.status === 200) {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            const pageTitle = $('title').text()
+
+            const links = [];
+            const imgs = [];
+
+            $('a').each((index, element) => {
+                const link = $(element).attr('href')
+                links.push(link)
+            })
+
+            $('img').each((index, element) => {
+                const img = $(element).attr('src')
+                imgs.push(img)
+            })
+
+
+            res.send(`
+                <h1>${pageTitle}</h1>
+                <h2>Enlaces</h2>
+                <ul>
+                    ${links.map(link => `<li><a href="${url}${link}">${link}</a></li>`).join('')}
+                </ul>
+
+                <h2>Imágenes</h2>
+                <ul>
+                    ${imgs.map(img => `<li><a href="${url}${img}">${img}</a></li>`).join('')}
+                </ul>
+                `)
+        }
+    })
+})
+
+app.listen(3000, () => {
+    console.log('express está escuchando en el puerto http://localhost:3000')
+})
